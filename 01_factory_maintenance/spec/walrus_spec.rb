23884bfdbac3fb8e8e51745d6d3fd0e085d6ca33
Joe Sak <joe@joesak.com>
2012-01-13 14:37:16 -0600

    Add the Factory Maintenance example WITHOUT proper factory

---
+++ 01_factory_maintenance/spec/walrus_spec.rb
@@ -0,0 +1,30 @@
+require 'factory_girl'
+require 'active_model'
+
+FactoryGirl.define do
+  factory :walrus do
+    id 1
+  end
+end
+
+class Walrus
+  include ActiveModel::Validations
+  attr_accessor :id, :name
+
+  validates :id, :numericality => true
+  validates :name, :presence => true
+end
+
+describe Walrus do
+  it "rejects an invalid id number" do
+    Factory.build(:walrus, :id => 'words').should_not be_valid
+  end
+
+  it "accepts a valid id number" do
+    Factory.build(:walrus, :id => 1).should be_valid
+  end
+
+  it "requires a name" do
+    Factory.build(:walrus, :name => nil).should_not be_valid
+  end
+end
